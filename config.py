a71702ed65dc7b843a349efa8cc5884a3a024d85
surajislam <154444093+surajislam@users.noreply.github.com>
2024-06-08 23:18:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "22256806"
+API_ID = "24547299"
 # -------------------------------------------------------------
-API_HASH = "92ab7ec36d41ae6316c5031b0f2b5749"
+API_HASH = "eb10c125d96d2146dc893b744470cea4"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = getenv("OWNER_ID", "6759076369")
-SUPPORT_GRP = "pikachuworld0099"
-UPDATE_CHNL = "anokhikeduniya"
-OWNER_USERNAME = "ll_SB_ANGLE_ll"
+OWNER_ID = getenv("OWNER_ID", "6891489621")
+SUPPORT_GRP = "https://t.me/+GEWO3YckXsZmZWRl"
+UPDATE_CHNL = "https://t.me/+GEWO3YckXsZmZWRl"
+OWNER_USERNAME = "II_AJJU_II"
 
